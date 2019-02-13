a7b643adc12125f55ba6bc526ad8352517db9f74
Dan Kolbman <dankolbman@gmail.com>
2019-02-13 14:18:41 +0000

    :bug: Handle keys with spaces

--- migration/04_delete.sh
+++ migration/04_delete.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cat $BUCKET/to_delete.csv \
-  | awk -F ',' '{print "{Key="$2",VersionId="$1"}"}' \
-  | gxargs -n100 \
-  | sed 's/ /,/g' \
-  | gxargs -L1 -I {} aws s3api delete-objects --bucket $BUCKET --delete Objects=\[{}\],Quiet=false
+  | awk -F ',' '{print "{Key="$3",VersionId="$2"}"}' \
+  | xargs -L100 \
+  | sed 's/} {/},{/g' \
+  | xargs -L1 -I {} aws s3api delete-objects --bucket $BUCKET --delete Objects=\[{}\],Quiet=false
